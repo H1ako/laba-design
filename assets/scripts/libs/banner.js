82a035e9a2b36ae14b753e55a145a7a40c1c28e9
h1ako <nikita-sobolev-wd@yandex.ru>
2025-03-13 21:33:42 +0400

    Update banner scene transition logic to ensure consistent scene cycling

--- assets/scripts/libs/banner.js
+++ assets/scripts/libs/banner.js
@@ -3,6 +3,6 @@ const banner = document.querySelector('.banner')
 setInterval(() => {
     const currentScene = Number(banner.dataset.scene)
     const nextScene = currentScene === 3 ? 1 : currentScene + 1
+    
     banner.dataset.scene = nextScene
-
 }, 5000)
