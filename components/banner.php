e14c5cb033729f8cb5795cc1cb9dbdb39addac8f
h1ako <nikita-sobolev-wd@yandex.ru>
2025-03-13 20:35:28 +0400

    Add background animation and scale effects to banner component

--- components/banner.php
+++ components/banner.php
@@ -1,4 +1,7 @@
 <div class="banner" data-scene="1">
+    <div class="banner__bg">
+        <?php include('components/bg-part-2.php'); ?>
+    </div>
     <div class="banner__logo">
         <?php include_once('components/logo.php'); ?>
     </div>
@@ -6,15 +9,15 @@
         <div class="content__scene scene_1">
             <img class="scene__image" src="<?= $SITE_URL ?>/assets/images/military.png" alt="Military">
             <p class="scene__text">Подпиши контракт по нашему рефералу и получи промокод на скидку 50%</p>
-            <a href="" class="scene__button">подробнее</a>
+            <a target="_blank" href="" class="scene__button">подробнее</a>
         </div>
         <div class="content__scene scene_2">
             <p class="scene__text">Время приёма заказов: круглосуточно 7 дней в неделю.</p>
-            <div class="scene__phone">+7 (912) 123-45-67</div>
+            <a href="tel:79121234567" class="scene__phone">+7 (912) 123-45-67</a>
         </div>
         <div class="content__scene scene_3">
             <p class="scene__text">Скидка на весь ассортимент 30%</p>
-            <a href="<?= Router::getRoute('/catalog') ?>" class="scene__button">перейти в каталог</a>
+            <a target="_blank" href="<?= Router::getRoute('/catalog') ?>" class="scene__button">перейти в каталог</a>
         </div>
     </div>
 </div>
