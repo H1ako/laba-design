6dd1b36166d15df345f1beb56f9e64cfe5e26a31
h1ako <nikita-sobolev-wd@yandex.ru>
2025-03-14 10:28:17 +0400

    Update banner link to direct users to the military website

--- components/banner.php
+++ components/banner.php
@@ -9,7 +9,7 @@
         <div class="content__scene scene_1">
             <img class="scene__image" src="<?= $SITE_URL ?>/assets/images/military.png" alt="Military">
             <p class="scene__text">Подпиши контракт по нашему рефералу и получи промокод на скидку 50%</p>
-            <a target="_blank" href="" class="scene__button">подробнее</a>
+            <a target="_blank" href="https://mil.ru/" class="scene__button">подробнее</a>
         </div>
         <div class="content__scene scene_2">
             <p class="scene__text">Время приёма заказов: круглосуточно 7 дней в неделю.</p>
