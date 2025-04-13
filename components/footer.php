a6c2a901f3431f52979b4d7aec214676a2e026dd
h1ako <nikita-sobolev-wd@yandex.ru>
2025-04-13 22:08:08 +0400

    feat: Update footer component to dynamically display published company pages

--- components/footer.php
+++ components/footer.php
@@ -1,4 +1,14 @@
-<footer class="main-footer" aria-label="Подвал сайта">
+<?php
+
+use app\models\CustomPage;
+
+// Get published pages for menu
+$company_pages = CustomPage::where('is_published', '=', 1)
+    ->order_by('sort_order', 'ASC')
+    ->get();
+?>
+
+<footer class="main-footer" aria-label="Подвал сайта">
     <div class="main-footer__container">
         <div class="container__content">
             <div class="content__col col_socials">
@@ -17,24 +27,20 @@
                 </div>
             </div>
             <div class="content__col col_nav">
-                <p class="content__title">For Users</p>
+                <p class="content__title">Company</p>
                 <nav class="content__nav" aria-label="Навигация пользователя">
                     <ul class="nav__list">
-                        <li class="list__el">
-                            <a href="#" class="el__link">Политика конфиденциальности</a>
-                        </li>
-                        <li class="list__el">
-                            <a href="#" class="el__link">Как работаем</a>
-                        </li>
-                        <li class="list__el">
-                            <a href="#" class="el__link">Обработка заказов</a>
-                        </li>
-                        <li class="list__el">
-                            <a href="#" class="el__link">Скидки и цены</a>
-                        </li>
-                        <li class="list__el">
-                            <a href="#" class="el__link">Контакты</a>
-                        </li>
+                        <?php foreach ($company_pages as $page): ?>
+                            <li class="list__el">
+                                <a href="<?= Router::getRoute('/page/' . $page->slug) ?>" class="el__link"><?= htmlspecialchars($page->title) ?></a>
+                            </li>
+                        <?php endforeach; ?>
+
+                        <?php if (count($company_pages) === 0): ?>
+                            <li class="list__el">
+                                <span class="el__link disabled">Нет доступных страниц</span>
+                            </li>
+                        <?php endif; ?>
                     </ul>
                 </nav>
             </div>
